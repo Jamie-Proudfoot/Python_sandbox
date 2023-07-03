d694173fb12dcd7effd6ad6ad8389af8fd44022d
J.Proudfoot <57542646+Jamie-Proudfoot@users.noreply.github.com>
2023-07-03 14:14:58 +0100

    Update naming convention (Z->A)

--- leep_sandbox.py
+++ leep_sandbox.py
@@ -7,7 +7,7 @@ import datetime
 
 # Original code. Author: youkaichao. Github: https://github.com/thuml/LogME/blob/main/LEEP.py
 
-def LEEP(Z: np.ndarray, Y: np.ndarray):
+def LEEP(A: np.ndarray, Y: np.ndarray):
     """
 
     :param pseudo_source_label: shape [N, C_s]
@@ -15,7 +15,7 @@ def LEEP(Z: np.ndarray, Y: np.ndarray):
     :return: leep score
     """
     # added for consistent notation
-    pseudo_source_label = Z
+    pseudo_source_label = A
     target_label = Y
     # added for consistent notation
     N, C_s = pseudo_source_label.shape
@@ -38,28 +38,28 @@ def LEEP(Z: np.ndarray, Y: np.ndarray):
 
 # Modified code (verbose). Author: JamieProudfoot.
 
-def LEEP_verbose(Z,Y):
+def LEEP_verbose(A,Y):
     """
-    Z :: pseudo source label (n*k_z matrix)
+    A :: pseudo source label (n*k_z matrix)
     Y :: target label (n vector)
     returns leep :: numerical meausure of source model 
     to target dataset transferability
     verbose version
     """
     # n: number of data points, k_z: number of pseudo source label classes
-    n, k_z = Z.shape
+    n, k_z = A.shape
     # Normalise Z so that total sum over Z == 1
-    normalised_Z = Z / n
+    normalised_A = A / n
     # k_y: number of target label classes
     k_y=int(np.max(Y) + 1)
     # Initialise joint probability matrix P(y,z)
     joint=np.zeros((k_y, k_z))
     # Single loop over target data classes
-    for y in range(k_y): joint[y] = np.sum(normalised_Z[Y == y], axis=0)
+    for y in range(k_y): joint[y] = np.sum(normalised_A[Y == y], axis=0)
     # Compute conditional probability matrix P(y|z) = P(y,z) / P(z)
     conditional = (joint / joint.sum(axis=0)).T
     # Compute EEP (expected empirical prediction)
-    marginal = Z @ conditional
+    marginal = A @ conditional
     EEP = np.array([py[y] for py, y in zip(marginal, Y)])
     # Compute LEEP (log of expected empirical prediction)
     leep = np.mean(np.log(EEP))
@@ -69,26 +69,26 @@ def LEEP_verbose(Z,Y):
 
 # Modified code (succinct). Author: JamieProudfoot.
 
-def LEEP_succinct(Z,Y):
+def LEEP_succinct(A,Y):
     """
-    Z :: pseudo source label (n*k_z matrix)
+    A :: pseudo source label (n*k_z matrix)
     Y :: target label (n vector)
     returns leep :: numerical meausure of source model 
     to target dataset transferability
     succinct version
     """
-    n,k_z=Z.shape
-    Zn=Z/n
+    n,k_z=A.shape
+    An=A/n
     k_y=int(np.max(Y)+1)
     joint=np.zeros((k_y,k_z))
     sort=np.argsort(Y)
     Ys=Y[sort]
     split=np.where(Ys[:-1]!=Ys[1:])[0]
-    joint=np.sum(np.array(np.split(Zn[sort],split+1)),axis=1)
+    joint=np.sum(np.array(np.split(An[sort],split+1)),axis=1)
     # joint=np.zeros((k_y, k_z))
-    # for y in range(k_y): joint[y]=np.sum(Zn[Y==y],axis=0)
+    # for y in range(k_y): joint[y]=np.sum(An[Y==y],axis=0)
     conditional=(joint/joint.sum(axis=0)).T
-    marginal=Z@conditional
+    marginal=A@conditional
     EEP=np.array([py[y] for py,y in zip(marginal,Y)])
     return np.mean(np.log(EEP))
 
@@ -96,25 +96,25 @@ def LEEP_succinct(Z,Y):
 
 # Testing equivalence of LEEP score functions
 
-Z = np.array([[0.3,0.7],[0.2,0.8],[0.9,0.1],[0.55,0.45]])
+A = np.array([[0.3,0.7],[0.2,0.8],[0.9,0.1],[0.55,0.45]])
 Y = np.array([1,1,0,0])
 
-print(f"Z:\n{Z}")
+print(f"A:\n{A}")
 print(f"Y:\n{Y}")
 print()
 
 t0 = datetime.datetime.now()
-print(f"Original: {LEEP(Z,Y)}")
+print(f"Original: {LEEP(A,Y)}")
 t1 = datetime.datetime.now()
 print((t1-t0))
 print()
 t0 = datetime.datetime.now()
-print(f"Verbose: {LEEP_verbose(Z,Y)}")
+print(f"Verbose: {LEEP_verbose(A,Y)}")
 t1 = datetime.datetime.now()
 print((t1-t0))
 print()
 t0 = datetime.datetime.now()
-print(f"Succinct: {LEEP_succinct(Z,Y)}")
+print(f"Succinct: {LEEP_succinct(A,Y)}")
 t1 = datetime.datetime.now()
 print((t1-t0))
 
